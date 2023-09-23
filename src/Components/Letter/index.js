56f100e56147a3c15b3cb4742634325e48a9fa31
ilyachase <ilya.chase@yandex.ru>
2023-09-23 12:02:02 +0500

    romaji as images alt

--- src/Components/Letter/index.js
+++ src/Components/Letter/index.js
@@ -1,7 +1,7 @@
 function Letter({randomLetter}) {
     return (
         <div className="mb-3">
-            <img src={'./Hiragana/' + randomLetter.id + '.svg'} width="450px" title={randomLetter.romaji} className="img-thumbnail"/>
+            <img src={'./Hiragana/' + randomLetter?.id + '.svg'} width="450px" alt={randomLetter?.romaji} title={randomLetter?.romaji} className="img-thumbnail"/>
         </div>
     );
 }
